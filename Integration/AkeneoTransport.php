f0d2be41a5f2fb7fa25a12956f4fc6b5cff053a1
Sylvain Rayé <sly@diglin.com>
2020-04-14 07:58:52 +0200

    FEATURE: add productUnit discovery interface and replace previous implementation into the ProductDataConverter to allow to extends the produtcUnit import

--- Integration/AkeneoTransport.php
+++ Integration/AkeneoTransport.php
@@ -14,14 +14,17 @@ use Oro\Bundle\AkeneoBundle\Integration\Iterator\AttributeFamilyIterator;
 use Oro\Bundle\AkeneoBundle\Integration\Iterator\AttributeIterator;
 use Oro\Bundle\AkeneoBundle\Integration\Iterator\CategoryIterator;
 use Oro\Bundle\AkeneoBundle\Integration\Iterator\ProductIterator;
-use Oro\Bundle\AkeneoBundle\Integration\Iterator\ReferenceDataIterator;
+use Oro\Bundle\AkeneoBundle\ProductUnit\ProductUnitDiscoveryInterface;
 use Oro\Bundle\IntegrationBundle\Entity\Transport;
 use Oro\Bundle\MultiCurrencyBundle\Config\MultiCurrencyConfigProvider;
+use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Intl\Intl;
 
 class AkeneoTransport implements AkeneoTransportInterface
 {
+    use LoggerAwareTrait;
+
     const PAGE_SIZE = 100;
 
     /**
@@ -63,11 +66,6 @@ class AkeneoTransport implements AkeneoTransportInterface
      * @var Filesystem
      */
     private $filesystem;
-
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
 
     public function __construct(
         AkeneoClientFactory $clientFactory,
